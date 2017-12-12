475675942115e72a85b94108cfa85a100e28a02e
Chandler Morrison <cwm795@gmail.com>
2017-12-12 09:21:18 -0600

    removed numbers greater than 19

--- main.js
+++ main.js
@@ -1,6 +1,6 @@
 var integers = [13, 25, 6, 3, 11, 2, 18, 7, 21, 1, 29, 20, 12, 8];
 
-
+// decending order
 function decending() {
     integers.sort((start, end) => {
       return end - start;
@@ -9,3 +9,9 @@ function decending() {
     console.log(" numbers in decending order ")
 }
 decending();
+
+// lesser than 19
+let result = integers.filter( (x) => {
+  return x < 19;
+  });
+console.log(result, "result");
