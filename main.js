37e606e8b464727ca86da583239ae4478d1bdb86
Chandler Morrison <cwm795@gmail.com>
2017-12-12 10:27:52 -0600

    multiplied by 1.5 and subtracted 1

--- main.js
+++ main.js
@@ -1,17 +1,10 @@
 var integers = [13, 25, 6, 3, 11, 2, 18, 7, 21, 1, 29, 20, 12, 8];
 
-// decending order
-function decending() {
-    integers.sort((start, end) => {
-      return end - start;
-    });
-    document.getElementById("div").innerHTML = integers;
-    console.log(" numbers in decending order ")
-}
-decending();
 
-// lesser than 19
-let result = integers.filter( (x) => {
-  return x < 19;
-  });
+let result = integers.sort((start, end) => {return end - start}).filter(x => {return x < 19;}).map((y) => y * 1.5 - 1);
 console.log(result, "result");
+
+// Multiply each remaining number by 1.5 and then subtract 1.
+
+
+
