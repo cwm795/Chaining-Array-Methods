d47da356d9bf7f8b777acf5c3eda227500e8a76f
Chandler Morrison <cwm795@gmail.com>
2017-12-12 10:41:59 -0600

    finished. result is in the console.

--- main.js
+++ main.js
@@ -1,10 +1,12 @@
 var integers = [13, 25, 6, 3, 11, 2, 18, 7, 21, 1, 29, 20, 12, 8];
 
 
-let result = integers.sort((start, end) => {return end - start}).filter(x => {return x < 19;}).map((y) => y * 1.5 - 1);
+let result = integers.sort((start, end) => {return end - start})
+.filter(x => {return x < 19;})
+.map((y) => y * 1.5 - 1)
+.reduce((a, b) => a + b);
 console.log(result, "result");
 
-// Multiply each remaining number by 1.5 and then subtract 1.
 
 
 
